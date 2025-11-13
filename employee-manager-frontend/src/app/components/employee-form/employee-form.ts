f6be3795dc7fa09b9de23b629c7387ffc8836d3c
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2025-11-13 14:43:47 +0300

    employee-form updated

--- employee-manager-frontend/src/app/components/employee-form/employee-form.ts
+++ employee-manager-frontend/src/app/components/employee-form/employee-form.ts
@@ -1,11 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, Output, EventEmitter, NgModule } from '@angular/core';
+import { Employee } from '../../models/employee';
+import { EmployeeService } from '../../services/employee-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee-form.html',
   styleUrl: './employee-form.css',
 })
-export class EmployeeForm {
+export class EmployeeForm implements OnChanges{
+
+  @Input() employee? : Employee;
+  @Output() employeeSaved = new EventEmitter<Employee>();
+
+  model : Employee = this.getEmptyEmployee();
+  isEditing = false;
+
+  constructor(private employeeService : EmployeeService){}
+
+  ngOnChanges(): void {
+    if(this.employee){
+      this.model = {... this.employee};
+      this.isEditing = true;
+    }else{
+      this.model = this.getEmptyEmployee();
+      this.isEditing = false;
+    }
+}
+
+private getEmptyEmployee() : Employee{
+return{
+  id: 0,
+  name: '',
+  email:'',
+  jobTitle:'',
+  phoneNumber:'',
+  imageUrl:'',
+  employeeCode:'' 
+};
+}
+
+onSubmit() : void{
+  if(this.isEditing){
+    this.employeeService.updateEmployee(this.model.id, this.model).subscribe({
+      next : (employee) => {
+        this.employeeSaved.emit(employee);
+        this.resetForm();
+      },
+      error : (error) => {
+        console.error("Updating Employee Error: ", error);
+      }
+    });
+  }else{
+    this.employeeService.addEmployee(this.model).subscribe({
+      next : (employee) => {
+        this.employeeSaved.emit(employee);
+        this.resetForm();
+      },
+      error : (error) => {
+        console.error("Creating Employee Error: ", error);
+      }
+    });
+  }
+}
+
+resetForm():void{
+this.model = this.getEmptyEmployee();
+this.isEditing = false;
+}
 
 }
