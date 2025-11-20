2053a3e45bf9d71766ea7929ca698d2c0068b28a
Seyyit Battal Arvas <seyyitbattalarvas3@gmail.com>
2025-11-20 14:55:29 +0300

    DB connection done. Will continue for be better.

--- src/main/java/com/ik/employeeManager/service/EmployeeService.java
+++ src/main/java/com/ik/employeeManager/service/EmployeeService.java
@@ -12,29 +12,6 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-  /* DB BAĞLANTISI YAPILDIKTAN SONRA BU KODLARA İHTİYAÇ YOK!
-
-    private final Map<Long, Employee> employees =  new HashMap<>();
-    private final AtomicLong nextId = new AtomicLong(1);
-    @PostConstruct
-    public void init(){
-        addEmployee(new Employee(0, "Ali Al", "ali@ali.com",
-                "Junior Developer", "05050505050",
-                "https://www.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg",
-                "EMP1245"));
-
-        addEmployee(new Employee(0, "Ayşe Ay", "ayse@ayse.com",
-                "Manager", "111111",
-                "https://img.freepik.com/premium-vector/business-woman-character-vector-illustration_1133257-2432.jpg?semt=ais_hybrid&w=740&q=80",
-                "EMP8964"));
-
-        addEmployee(new Employee(0, "Veli Vel", "vel@vel.com",
-                "IT Director", "921921291",
-                "",
-                "EMP2378"));
-    }
-    */
-
     @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
@@ -49,6 +26,7 @@ public class EmployeeService {
     }
 
     public Employee addEmployee(Employee employee){
+        employee.setId(null);
         return employeeRepository.save(employee);
     }
 
